6174e0e374fc41b9c2d7f4abfe8e43799aaddd21
unknown <adityarastogi94c@gmail.com>
2020-04-20 05:28:46 +0530

    Changes made to make Heroku Work

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const message2 = document.getElementById('message-2')
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()  
     const location= search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {   message1.innerHTML=data.error
